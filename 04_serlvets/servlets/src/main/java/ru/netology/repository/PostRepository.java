37a80e6fcbb6f5d0cbfc9c940f45e73ddd294a5a
Valeriy Shubin <b-boy56@mail.ru>
2024-02-11 13:55:23 +0300

    complete task 1

--- 04_serlvets/servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -2,24 +2,52 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepository {
-  public List<Post> all() {
-    return Collections.emptyList();
-  }
+    private final Map<Long, Post> postsMap;
+    private final AtomicLong baseID = new AtomicLong(1);
 
-  public Optional<Post> getById(long id) {
-    return Optional.empty();
-  }
+    public PostRepository() {
+        postsMap = new ConcurrentHashMap<>();
+    }
 
-  public Post save(Post post) {
-    return post;
-  }
+    public List<Post> all() {
+        return new ArrayList<>(postsMap.values());
+    }
 
-  public void removeById(long id) {
-  }
+    public Optional<Post> getById(long id) {
+        if (postsMap.containsKey(id)) {
+            return Optional.ofNullable(postsMap.get(id));
+        }
+        return Optional.empty();
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            while (postsMap.containsKey(baseID.get())) {
+                baseID.getAndIncrement();
+            }
+            post.setId(baseID.get());
+            postsMap.put(baseID.get(), post);
+        } else {
+            postsMap.put(post.getId(), post);
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        if (postsMap.containsKey(id)) {
+            postsMap.remove(id);
+            if (id < baseID.get()) {
+                baseID.set(id);
+            }
+        }
+    }
 }
